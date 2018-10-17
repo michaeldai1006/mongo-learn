d1a5ecc33a832b8409e53f5a93988feb0eeb41c0
michaeldai1006 <dtc74110@gmail.com>
2018-10-17 10:11:59 -0700

    MongoDB connection

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
-
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/mongo-learn', { useNewUrlParser: true })
+  .catch((err) => { console.log(err); });
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
 
